5eb6d9e124e9f1efa6a340552e5e708a7e1b738c
sumolx <sumolx@47506847-fb4f-4ee6-b66f-4fd32c0f12a1>
2011-02-23 00:41:49 +0000

    Moved tools from Engine to its own path.

--- engine/tools/borplay/source/build-linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Builds borplay. Note that it has a dependency on libao. To build, you must 
-# have libao-dev installed.  To install it on Debian and Ubuntu, install the 
-# package libao-dev.
-
-gcc adpcm.c borplay.c kbhit.c -lao -o ../borplay
-
