c00c93f86ee73dc4c043f25d7523648243c64314
anallyst <anallyst@47506847-fb4f-4ee6-b66f-4fd32c0f12a1>
2011-02-25 03:22:00 +0000

    use real malloc instead of proxy function in non-debug mode

--- engine/source/tracelib/tracemalloc.h
+++ engine/source/tracelib/tracemalloc.h
@@ -12,6 +12,7 @@
 #define TRACEMALLOC_H
 
 #include <string.h>
+#include <stdlib.h>
 
 #ifdef DEBUG
 #ifndef NO_RAM_DEBUGGER
@@ -24,11 +25,17 @@
 extern size_t tracemalloc_total;
 
 /////////////////////////////////////////////////////////////////////////////
-
+#ifndef RAM_DEBUG
+#define tracemalloc(a,b) malloc(b)
+#define tracerealloc(a,b) realloc(a,b)
+#define tracefree(a) free(a)
+#define tracecalloc(a,b) calloc(1,b)
+#else
 void *tracemalloc(const char *name, size_t len);
 void *tracecalloc(const char *name, size_t len);
 void *tracerealloc(void *p, size_t len);
 void tracefree(void *p);
+#endif
 int tracemalloc_dump(void);
 
 /////////////////////////////////////////////////////////////////////////////
