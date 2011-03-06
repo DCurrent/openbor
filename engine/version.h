956e6a9c3c56116d7189f53983ea2e019c66e6a1
sumolx <SumolX@users.noreply.github.com>
2011-03-06 16:16:15 +0000

    libpng wrapper functions not need for loadimage.  However, we still need the wrapper for png_set_expand_gray_1_2_4_to_8 as it was introduced in libpng 1.2.9 and Dreamcast uses build 1.2.8

--- engine/version.h
+++ engine/version.h
@@ -12,7 +12,7 @@
 #define VERSION_NAME "OpenBOR"
 #define VERSION_MAJOR "3"
 #define VERSION_MINOR "0"
-#define VERSION_BUILD "3138"
+#define VERSION_BUILD "3141"
 #define VERSION ("v"VERSION_MAJOR"."VERSION_MINOR" Build "VERSION_BUILD)
 
 #endif
