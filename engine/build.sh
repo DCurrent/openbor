a040351fd99780a906c2f85c081b19f7406743c5
sumolx <SumolX@users.noreply.github.com>
2011-03-10 02:59:31 +0000

    Fixed execution of calling borpak's build.sh via build.sh from engine's auto build script

--- engine/build.sh
+++ engine/build.sh
@@ -48,10 +48,10 @@ function distribute {
     exit 1
   fi
   if test -e "releases/WINDOWS/OpenBOR/OpenBOR.exe"; then
-    ../tools/borpak/source/build.sh win
-	cp ../tools/borpak/source/borpak.exe releases/WINDOWS/OpenBOR/
-	cp ../tools/borpak/scripts/packer.bat releases/WINDOWS/OpenBOR/
-	cp ../tools/borpak/scripts/paxplode.bat releases/WINDOWS/OpenBOR/
+    . ../tools/borpak/source/build.sh win
+    cp ../tools/borpak/source/borpak.exe releases/WINDOWS/OpenBOR/
+    cp ../tools/borpak/scripts/packer.bat releases/WINDOWS/OpenBOR/
+    cp ../tools/borpak/scripts/paxplode.bat releases/WINDOWS/OpenBOR/
   else
     echo "Windows Platform Failed To Build!"
     exit 1  
@@ -65,10 +65,10 @@ function distribute {
     # exit 1
   fi
   if test -e "releases/LINUX/OpenBOR/OpenBOR"; then
-    ../tools/borpak/source/build.sh lin
-	cp ../tools/borpak/source/borpak releases/LINUX/OpenBOR/
-	cp ../tools/borpak/scripts/packer releases/LINUX/OpenBOR/
-	cp ../tools/borpak/scripts/paxplode releases/LINUX/OpenBOR/
+    . ../tools/borpak/source/build.sh lin
+    cp ../tools/borpak/source/borpak releases/LINUX/OpenBOR/
+    cp ../tools/borpak/scripts/packer releases/LINUX/OpenBOR/
+    cp ../tools/borpak/scripts/paxplode releases/LINUX/OpenBOR/
   else
     if [ `echo $HOST_PLATFORM | grep -o "Linux"` ]; then
       echo "Linux Platform Failed To Build!"
@@ -76,10 +76,10 @@ function distribute {
     fi
   fi
   if test -e "releases/DARWIN/OpenBOR.app/Contents/MacOS/OpenBOR"; then
-    ../tools/borpak/source/build.sh mac
-	cp ../tools/borpak/source/borpak releases/DARWIN/OpenBOR.app/Contents/MacOS/
-	cp ../tools/borpak/scripts/packer releases/DARWIN/OpenBOR.app/Contents/MacOS/
-	cp ../tools/borpak/scripts/paxplode releases/DARWIN/OpenBOR.app/Contents/MacOS/
+    . ../tools/borpak/source/build.sh mac
+    cp ../tools/borpak/source/borpak releases/DARWIN/OpenBOR.app/Contents/MacOS/
+    cp ../tools/borpak/scripts/packer releases/DARWIN/OpenBOR.app/Contents/MacOS/
+    cp ../tools/borpak/scripts/paxplode releases/DARWIN/OpenBOR.app/Contents/MacOS/
   else
     if [ `echo $HOST_PLATFORM | grep -o "Darwin"` ]; then
       echo "Darwin Platform Failed To Build!"
