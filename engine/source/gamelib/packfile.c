0a26f4db9f51b3c5a2ac889941a8214efbc42052
utunnels <utunnels@47506847-fb4f-4ee6-b66f-4fd32c0f12a1>
2011-09-02 01:33:48 +0000

    Remove cd header size checking in packfile.c Large pak file shouldn't be able to play now.

--- engine/source/gamelib/packfile.c
+++ engine/source/gamelib/packfile.c
@@ -1054,6 +1054,7 @@ int pak_init()
 		// let's cache it on CD sector boundaries
 		int pak_cdheaderstart = pak_headerstart & (~0x7FF);
 		int pak_cdheadersize = ((paksize - pak_cdheaderstart) + 0x7FF) & (~0x7FF);
+		#if 0
 		if(pak_cdheadersize > 524288)
 		{
 			// Original value was 262144, which has been doubled.
@@ -1064,6 +1065,7 @@ int pak_init()
 			printf("pak header is too large: %d\n",pak_cdheadersize);
 			return 0;
 		}
+		#endif
 		pak_cdheader = malloc(pak_cdheadersize);
 		if(!pak_cdheader) { printf("pak_cdheader malloc failed\n"); return 0; }
 		if(pak_getsectors(pak_cdheader, pak_cdheaderstart >> 11, pak_cdheadersize >> 11) != (pak_cdheadersize >> 11))
