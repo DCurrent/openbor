fd40e450a2db08fee3ae9f8a48c4f68d7c3ed928
Bryan Cain <bryancain3@gmail.com>
2012-10-26 21:53:18 +0000

    Complete rewrite of Simple 2x scaling filter to remove more GPL-licensed code from the engine.  The implementation ended up being very similar, even though I didn't look at the original code until after finishing my own implementation.

--- engine/source/gfxlib/simple2x.c
+++ engine/source/gfxlib/simple2x.c
@@ -1,119 +1,63 @@
-// VisualBoyAdvance - Nintendo Gameboy/GameboyAdvance (TM) emulator.
-// Copyright (C) 1999-2003 Forgotten
-// Copyright (C) 2004 Forgotten and the VBA development team
-
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2, or(at your option)
-// any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software Foundation,
-// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
 /*
- * Code adapted To OpenBOR by SX
- * simple2x.c - Trying to scale 2x.
+ * OpenBOR - http://www.LavaLit.com
+ * -----------------------------------------------------------------------
+ * All rights reserved, see LICENSE in OpenBOR root for details.
  *
- * Updated: 5/05/08 - SX
- *
+ * Copyright (c) 2004 - 2012 OpenBOR Team
  */
 
-#include "borendian.h"
-#include "gfxtypes.h"
+/**
+ * Scales images by 2x using nearest neighbor filtering.  Replaces the old
+ * "Simple 2x" filter used in OpenBOR before October 2012, which was licensed
+ * under the GPL.
+ */
+
+#include "gfx.h"
+#include "types.h"
 
 void Simple2x(u8 *srcPtr, u32 srcPitch, u8 *deltaPtr, u8 *dstPtr, u32 dstPitch, int width, int height)
 {
-	u8 *nextLine, *finish;
-
-	nextLine = dstPtr + dstPitch;
-
-	do
+	u16 *srcPixels;
+	u16 *dstTop, *dstBottom;
+	int x, y;
+	
+	for (y=0; y<height; y++)
 	{
-		u32 *bP = (u32 *) srcPtr;
-		u32 *dP = (u32 *) dstPtr;
-		u32 *nL = (u32 *) nextLine;
-		u32 currentPixel;
-
-		finish = (u8 *) bP + ((width+2) << 1);
-		currentPixel = *bP++;
-
-		do
+		srcPixels = (u16*)(srcPtr + y*srcPitch);
+		dstTop = (u16*)dstPtr;
+		dstBottom = (u16*)(dstPtr + dstPitch);
+		for(x=0; x<width; x++)
 		{
-#ifdef BOR_BIG_ENDIAN
-			u32 color = currentPixel >> 16;
-#else
-		    u32 color = currentPixel & 0xffff;
-#endif
-
-			color = color | (color << 16);
-
-			*(dP) = color;
-			*(nL) = color;
-
-#ifdef BOR_BIG_ENDIAN
-			color = currentPixel & 0xffff;
-#else
-			color = currentPixel >> 16;
-#endif
-			color = color| (color << 16);
-			*(dP + 1) = color;
-			*(nL + 1) = color;
-
-			currentPixel = *bP++;
-
-			dP += 2;
-			nL += 2;
+			*dstTop++ = *srcPixels;
+			*dstTop++ = *srcPixels;
+			*dstBottom++ = *srcPixels;
+			*dstBottom++ = *srcPixels;
+			srcPixels++;
 		}
-		while ((u8 *) bP < finish);
-
-		srcPtr += srcPitch;
 		dstPtr += dstPitch << 1;
-		nextLine += dstPitch << 1;
 	}
-	while (--height);
 }
 
 void Simple2x32(u8 *srcPtr, u32 srcPitch, u8 *deltaPtr, u8 *dstPtr, u32 dstPitch, int width, int height)
 {
-	u8 *nextLine, *finish;
-
-	nextLine = dstPtr + dstPitch;
-
-	do
+	u32 *srcPixels;
+	u32 *dstTop, *dstBottom;
+	int x, y;
+	
+	for (y=0; y<height; y++)
 	{
-		u32 *bP = (u32 *) srcPtr;
-	    u32 *dP = (u32 *) dstPtr;
-		u32 *nL = (u32 *) nextLine;
-		u32 currentPixel;
-
-		finish = (u8 *) bP + ((width+1) << 2);
-		currentPixel = *bP++;
-
-		do
+		srcPixels = (u32*)(srcPtr + y*srcPitch);
+		dstTop = (u32*)dstPtr;
+		dstBottom = (u32*)(dstPtr + dstPitch);
+		for(x=0; x<width; x++)
 		{
-			u32 color = currentPixel;
-
-		    *(dP) = color;
-			*(dP+1) = color;
-			*(nL) = color;
-			*(nL + 1) = color;
-
-			currentPixel = *bP++;
-
-			dP += 2;
-			nL += 2;
+			*dstTop++ = *srcPixels;
+			*dstTop++ = *srcPixels;
+			*dstBottom++ = *srcPixels;
+			*dstBottom++ = *srcPixels;
+			srcPixels++;
 		}
-		while ((u8 *) bP < finish);
-
-		srcPtr += srcPitch;
 		dstPtr += dstPitch << 1;
-		nextLine += dstPitch << 1;
 	}
-	while (--height);
 }
+
