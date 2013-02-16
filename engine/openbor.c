1f5678eca72888f834c27f9887b4bc2d81412ffc
utunnels <utunnels@users.noreply.github.com>
2013-02-16 07:54:40 +0000

    Fix select screen plays sleep animation bug.

--- engine/openbor.c
+++ engine/openbor.c
@@ -10933,7 +10933,7 @@ int common_idle_anim(entity* ent)
 		ent_set_anim(ent, ANI_FAINT, 0);                                                //Set ANI_FAINT.
 		goto found;                                                                      //Return 1 and exit.
 	}
-	else if(validanim(ent,ANI_SLEEP) && time >= ent->sleeptime)     //ANI_SLEEP, sleeptime up
+	else if(validanim(ent,ANI_SLEEP) && time > ent->sleeptime)     //ANI_SLEEP, sleeptime up
 	{
 		ent_set_anim(ent, ANI_SLEEP, 0);                                                //Set sleep anim.
 		goto found;                                                                     //Return 1 and exit.
