7792b1407eb4541489d8bcfd87114bfb993cf727
utunnels <utunnels@47506847-fb4f-4ee6-b66f-4fd32c0f12a1>
2013-04-30 02:00:04 +0000

    Add another bit flag for frozen (2 means do not automatically drop).

--- engine/openbor.c
+++ engine/openbor.c
@@ -15291,14 +15291,14 @@ void checkdamageeffects(s_attack* attack)
 		if(opp->health > opp->modeldata.health)
 			opp->health = opp->modeldata.health;
 	}
-	if(_freeze && !self->frozen && !self->owner && !self->modeldata.nomove)
+	if(_freeze && !self->frozen)
 	{    // New freeze attack - If not frozen, freeze entity unless it's a projectile
-		self->frozen = 1;
+		self->frozen = _freeze;
 		if(self->freezetime == 0) self->freezetime = time + _freezetime;
 		if(_remap == -1 && self->modeldata.maps.frozen != -1) self->colourmap = model_get_colourmap(&(self->modeldata),self->modeldata.maps.frozen);    //12/14/2007 Damon Caskey: If opponents frozen map = -1 or only stun, then don't change the color map.
 		self->drop = 0;
 	}
-	else if(self->frozen)
+	else if(self->frozen && !(self->frozen&2))
 	{
 		unfrozen(self);
 		self->drop = 1;
