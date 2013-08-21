5a45e73cea1eb1cff3d8f53e1075f968abee2212
Bryan Cain <bryancain3@gmail.com>
2013-08-21 18:58:55 +0000

    Display PAK selection menu background with the correct colors on the Wii.

--- engine/source/pnglib/pngdec.c
+++ engine/source/pnglib/pngdec.c
@@ -78,7 +78,11 @@ s_screen *pngToScreen(const void *data)
     {
         png_set_tRNS_to_alpha(png_ptr);
     }
+#if WII
+    png_set_filler(png_ptr, 0xff, PNG_FILLER_BEFORE);
+#else
     png_set_filler(png_ptr, 0xff, PNG_FILLER_AFTER);
+#endif
 
     image = allocscreen(width, height, PIXEL_32);
     if(image == NULL)
