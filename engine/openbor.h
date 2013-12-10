c020c70bdfa692f2224c30199883c203b72b87e3
Damon V. Caskey <dc@caskeys.com>
2013-12-10 00:12:26 +0000

    s_layer now uses s_axis struct.

--- engine/openbor.h
+++ engine/openbor.h
@@ -1538,14 +1538,18 @@ typedef struct
     bgloldtype oldtype;
     int order;	//for panel order
     gfx_entry gfx;
-    int width;
-    int height;
-    float xratio;
-    float zratio;
-    int xoffset;
-    int zoffset;
-    int xspacing;
-    int zspacing;
+    //int width;
+    //int height;
+    s_axis_i size;  //Only x and a.
+    //float xratio;
+    //float zratio;
+    s_axis_f ratio; //Only x and z.
+    //int xoffset;
+    //int zoffset;
+    s_axis_i offset; //Only x and z.
+    //int xspacing;
+    //int zspacing;
+    s_axis_i spacing; //Only x and z.
     s_drawmethod drawmethod;
     float bgspeedratio;
     int enabled;
