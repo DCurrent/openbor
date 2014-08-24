d363087c03d97bc5ca61c22f5ba9fe8253c42ccb
Damon V. Caskey <dc@caskeys.com>
2014-08-24 14:34:56 +0000

    srand{seed} now also returns a random number. Global seed moved into a struct (let's get rid of as many unpackaged globals as we can).

--- engine/source/randlib/rand32.h
+++ engine/source/randlib/rand32.h
@@ -9,7 +9,14 @@
 #ifndef RAND32_H
 #define RAND32_H
 
-extern unsigned long seed;
+// 2014-08-24, DC: Package seed into a stuct so we don't have
+// more unstructured global variables floating around.
+typedef struct
+{
+    unsigned long seed;
+} s_rand;
+
+//extern unsigned long seed;
 
 unsigned int rand32(void);
 
