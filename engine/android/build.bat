a4f1c9d90cbd20455fe10689418eaf625ff68da1
crxtrdude <crxtrdude@47506847-fb4f-4ee6-b66f-4fd32c0f12a1>
2014-11-27 04:23:03 +0000

    - Android port version 1.3.2: Updated the static library for libpng to 1.6.15. Removed the libz static library because libpng is now set up so it does not need it anymore, (it instead uses the zlib already present in the Android OS) - Optimized PNGs and resources, making the resulting compiled EXE smaller. - Fixed build.bat, because I accidentally committed it with the bash setting to '5' (Windows compile only) instead of 'all'.

--- engine/android/build.bat
+++ engine/android/build.bat
@@ -3,14 +3,18 @@
 @rem -----------------------------------------------------------------------
 @rem All rights reserved, see LICENSE in OpenBOR root for details.
 @rem
-@rem Copyright (c) 2004 - 2011 OpenBOR Team
+@rem Copyright (c) 2004 - 2014 OpenBOR Team
 @rem
 
-@rem Building libraries from OpenBOR source...
-@rem
+@echo off
+@echo -----------------------------------------------------------------------
+@echo Building libraries from OpenBOR source...
+@echo -----------------------------------------------------------------------
 call ndk-build
-@rem Compiling APK ...
-@rem
+@echo -----------------------------------------------------------------------
+@echo Compiling APK ...
+@echo -----------------------------------------------------------------------
 call ant debug
+@echo -----------------------------------------------------------------------
 pause
 
