07ac45cd684f42c725535d3cb94e7cb94bbe5c15
Bryan Cain <bryancain3@gmail.com>
2015-03-16 21:58:57 +0000

    Clean up Wii video code.

--- engine/wii/video.h
+++ engine/wii/video.h
@@ -1,9 +1,9 @@
 /*
- * OpenBOR - http://www.LavaLit.com
+ * OpenBOR - http://www.ChronoCrash.com
  * -----------------------------------------------------------------------
  * Licensed under the BSD license, see LICENSE in OpenBOR root for details.
  *
- * Copyright (c) 2004 - 2011 OpenBOR Team
+ * Copyright (c) 2004 - 2015 OpenBOR Team
  */
 
 #ifndef VIDEO_H
@@ -18,11 +18,9 @@ int   video_copy_screen(s_screen*);
 void  video_clearscreen();
 void  video_draw_quad(int,int,int,int);
 void  video_stretch(int);
-void  video_swizzle_simple(const void*,void*,int,int);
 void  video_exit();
 
-void  copyscreen8(s_screen*);
-void  copyscreen16(s_screen*);
+void  video_swizzle_simple(const void*,void*,int,int);
 void  copyscreen32(s_screen*);
 
 #endif
