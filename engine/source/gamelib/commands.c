3b38e37204e065b824b0c3c52780e596b2ed98d9
White Dragon <000whitedragon000@gmail.com>
2016-10-19 17:55:54 +0000

    added the vertical/diagonal support for bg/fg layers
    usage:
    vbgspeed value dir
    vbgspeed param is similar to bgspeed (both used in level#.txt)
    
    use vbgspeed+bgspeed combo for diagonal levels

--- engine/source/gamelib/commands.c
+++ engine/source/gamelib/commands.c
@@ -480,6 +480,7 @@ List *createLevelCommandList(void)
     LIST_ADD(CMD_LEVEL_FACING, "facing");
     LIST_ADD(CMD_LEVEL_ROCK, "rock");
     LIST_ADD(CMD_LEVEL_BGSPEED, "bgspeed");
+    LIST_ADD(CMD_LEVEL_VBGSPEED, "vbgspeed");
     LIST_ADD(CMD_LEVEL_SCROLLSPEED, "scrollspeed");
     LIST_ADD(CMD_LEVEL_MIRROR, "mirror");
     LIST_ADD(CMD_LEVEL_BOSSMUSIC, "bossmusic");
