f67b9c1e7e5b0b8fa37d1a2a0c9959851c07ca59
White Dragon <000whitedragon000@gmail.com>
2016-11-12 18:28:56 +0000

    added gotomainmenu(flag) to go to main menu without tricks or pausemenu flag: gotomainmenu(flag); flag: 1 = no gameover, 2 = no halloffame, 3 = 1+2 flag doesn't affect set nogameover/nohof

--- engine/openborscript.h
+++ engine/openborscript.h
@@ -276,6 +276,7 @@ HRESULT openbor_executescript(ScriptVariant **varlist , ScriptVariant **pretvar,
 
 HRESULT openbor_loadgamefile(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
 HRESULT openbor_finishlevel(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
+HRESULT openbor_gotomainmenu(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
 HRESULT openbor_playgame(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
 HRESULT openbor_getrecordingstatus(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
 HRESULT openbor_recordinputs(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
