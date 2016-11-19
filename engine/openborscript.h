ac5f4fde9faefcda1c764d6187908696d9ca473d
Damon V. Caskey <dc@caskeys.com>
2016-11-19 13:05:27 +0000

    - Reorganize menu functions with Big-endian style naming.

--- engine/openborscript.h
+++ engine/openborscript.h
@@ -245,7 +245,7 @@ HRESULT openbor_getentity(ScriptVariant **varlist , ScriptVariant **pretvar, int
 HRESULT openbor_loadmodel(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
 HRESULT openbor_loadsprite(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
 HRESULT openbor_hallfame(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
-HRESULT openbor_options(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
+HRESULT openbor_menu_options(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
 HRESULT openbor_playgif(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
 HRESULT openbor_openanigif(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
 HRESULT openbor_decodeanigif(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
