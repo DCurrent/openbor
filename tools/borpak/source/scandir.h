00672158a036cd7822361f5583fa29f627605006
Damon V. Caskey <dc@caskeys.com>
2017-04-01 15:25:01 -0400

    Remove the tool chain merge - that wasn't such a great idea after all.

--- tools/borpak/source/scandir.h
+++
@@ -1,5 +0,0 @@
-int scandir(const char *dir, struct dirent ***namelist,
-						int (*select)(const struct dirent *),
-						int (*compar)(const struct dirent **, const struct dirent **));
-int alphasort(const struct dirent **a, const struct dirent **b);
-
