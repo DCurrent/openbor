8e1eedc7717702839aef81a2d4be44f543b0fa01
Damon V. Caskey <dc@caskeys.com>
2017-05-02 10:09:03 -0400

    Remove Pointless Check
    Not sure who added this check or why, but as is it did nothing at all.
    Thanks for the catch Coringao.

---
+++ engine/version.h
@@ -0,0 +1,18 @@
+/*
+ * OpenBOR - http://www.ChronoCrash.com
+ * -----------------------------------------------------------------------
+ * All rights reserved, see LICENSE in OpenBOR root for details.
+ *
+ * Copyright (c) 2004 - 2014 OpenBOR Team
+ */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_NAME "OpenBOR"
+#define VERSION_MAJOR "3"
+#define VERSION_MINOR "0"
+#define VERSION_BUILD ""
+#define VERSION "v"VERSION_MAJOR"."VERSION_MINOR" Build "VERSION_BUILD
+
+#endif
