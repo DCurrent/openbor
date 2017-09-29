DCurrent/openbor

commit b6e3f41316401da028ab3889a30143f56f30a9b9

Fix strings longer than 63 characters in script
Before, the engine would just crash if you tried to create a string longer than
63 characters. Now strings of any length are supported. Also, short strings are
now allocated for their actual length, so we don't allocate 64 bytes for a
string that only needs 2. This should save memory, though I haven't tested how
much.

engine/openbor.c
@@ -1207,7 +1207,7 @@ int getsyspropertybyindex(ScriptVariant *var, int index)
         if(currentScene)
         {
             ScriptVariant_ChangeType(var, VT_STR);
-            strcpy(StrCache_Get(var->strVal), currentScene);
+            var->strVal = StrCache_CreateNewFrom(currentScene);
         }
         else
         {
@@ -1248,13 +1248,13 @@ int getsyspropertybyindex(ScriptVariant *var, int index)
         break;
     case _sv_branchname:
         ScriptVariant_ChangeType(var, VT_STR);
-        strcpy(StrCache_Get(var->strVal), branch_name);
+        var->strVal = StrCache_CreateNewFrom(branch_name);
         break;
     case _sv_current_branch:
         ScriptVariant_ChangeType(var, VT_STR);
         if(level != NULL && levelsets && levelsets[current_set].levelorder && levelsets[current_set].levelorder[current_level].branchname)
         {
-            strcpy(StrCache_Get(var->strVal), levelsets[current_set].levelorder[current_level].branchname);
+            var->strVal = StrCache_CreateNewFrom(levelsets[current_set].levelorder[current_level].branchname);
         }
         else
         {
@@ -1262,9 +1262,13 @@ int getsyspropertybyindex(ScriptVariant *var, int index)
         }
         break;
     case _sv_pakname:
+    {
+        char tempstr[256];
+        getPakName(tempstr, -1);
         ScriptVariant_ChangeType(var, VT_STR);
-        getPakName(StrCache_Get(var->strVal), -1);
+        var->strVal = StrCache_CreateNewFrom(tempstr);
         break;
+    }
     case _sv_maxsoundchannels:
         ScriptVariant_ChangeType(var, VT_INTEGER);
         var->lVal = maxchannels();

engine/source/scriptlib/ScriptVariant.c
@@ -60,9 +60,7 @@ void StrCache_Init()
     strcache_index = malloc(sizeof(*strcache_index) * STRCACHE_INC);
     for(i = 0; i < STRCACHE_INC; i++)
     {
-        strcache[i].str = malloc(sizeof(CHAR) * (MAX_STR_VAR_LEN + 1));
-        strcache[i].str[0] = 0;
-        strcache[i].len = MAX_STR_VAR_LEN;
+        strcache[i].str = NULL;
         strcache_index[i] = i;
     }
     strcache_size = STRCACHE_INC;
@@ -87,11 +85,13 @@ void StrCache_Collect(int index)
         //if(strcache[index].len > MAX_STR_VAR_LEN)
         //	StrCache_Resize(index, MAX_STR_VAR_LEN);
         //assert(strcache_top+1<strcache_size);
+        free(strcache[index].str);
+        strcache[index].str = NULL;
         strcache_index[++strcache_top] = index;
     }
 }
 
-int StrCache_Pop()
+int StrCache_Pop(int length)
 {
     int i;
     if(strcache_size == 0)
@@ -105,9 +105,6 @@ int StrCache_Pop()
         for(i = 0; i < STRCACHE_INC; i++)
         {
             strcache_index[i] = strcache_size + i;
-            strcache[i + strcache_size].str = malloc(sizeof(CHAR) * (MAX_STR_VAR_LEN + 1));
-            strcache[i + strcache_size].str[0] = 0;
-            strcache[i + strcache_size].len = MAX_STR_VAR_LEN;
         }
 
         //printf("debug: dumping string cache....\n");
@@ -120,8 +117,19 @@ int StrCache_Pop()
         //printf("debug: string cache resized to %d \n", strcache_size);
     }
     i = strcache_index[strcache_top--];
+    strcache[i].str = malloc(length + 1);
+    strcache[i].str[0] = 0;
+    strcache[i].len = length;
     strcache[i].ref = 1;
     return i;
+}
+
+int StrCache_CreateNewFrom(const CHAR *str)
+{
+    int len = strlen(str);
+    int strVal = StrCache_Pop(len);
+    memcpy(StrCache_Get(strVal), str, len + 1);
+    return strVal;
 }
 
 void StrCache_Copy(int index, CHAR *str)
@@ -189,12 +197,7 @@ void ScriptVariant_ChangeType(ScriptVariant *var, VARTYPE cvt)
     {
         StrCache_Collect(var->strVal);
     }
-    if(cvt == VT_STR)
-    {
-        var->strVal = StrCache_Pop();
-    }
     var->vt = cvt;
-
 }
 
 // find an existing constant before copy
@@ -210,12 +213,12 @@ void ScriptVariant_ParseStringConstant(ScriptVariant *var, CHAR *str)
             var->strVal = i;
             strcache[i].ref++;
             var->vt = VT_STR;
-            break;
+            return;
         }
     }
 
     ScriptVariant_ChangeType(var, VT_STR);
-    StrCache_Copy(var->strVal, str);
+    var->strVal = StrCache_CreateNewFrom(str);
 }
 
 HRESULT ScriptVariant_IntegerValue(ScriptVariant *var, LONG *pVal)
@@ -294,6 +297,26 @@ void ScriptVariant_ToString(ScriptVariant *svar, LPSTR buffer )
     default:
         sprintf(buffer, "<Unprintable VARIANT type.>" );
         break;
+    }
+}
+
+static int ScriptVariant_LengthAsString(ScriptVariant *svar)
+{
+    switch (svar->vt)
+    {
+    case VT_EMPTY:
+        return snprintf(NULL, 0, "<VT_EMPTY>   Unitialized");
+    case VT_INTEGER:
+        return snprintf(NULL, 0, "%d", svar->lVal);
+    case VT_DECIMAL:
+        return snprintf(NULL, 0, "%lf", svar->dblVal);
+    case VT_PTR:
+        return snprintf(NULL, 0, "#%ld", (long)(svar->ptrVal));
+    case VT_STR:
+        // could use StrCache_Len instead to improve speed
+        return snprintf(NULL, 0, "%s", StrCache_Get(svar->strVal));
+    default:
+        return snprintf(NULL, 0, "<Unprintable VARIANT type.>");
     }
 }
 
@@ -672,7 +695,6 @@ ScriptVariant *ScriptVariant_Add( ScriptVariant *svar, ScriptVariant *rightChild
 {
     static ScriptVariant retvar = {{.ptrVal = NULL}, VT_EMPTY};
     DOUBLE dbl1, dbl2;
-    CHAR buf[MAX_STR_VAR_LEN + 1];
     if(ScriptVariant_DecimalValue(svar, &dbl1) == S_OK &&
             ScriptVariant_DecimalValue(rightChild, &dbl2) == S_OK)
     {
@@ -689,10 +711,16 @@ ScriptVariant *ScriptVariant_Add( ScriptVariant *svar, ScriptVariant *rightChild
     }
     else if(svar->vt == VT_STR || rightChild->vt == VT_STR)
     {
+        CHAR *dst;
+        int len1 = ScriptVariant_LengthAsString(svar),
+            len2 = ScriptVariant_LengthAsString(rightChild);
+
         ScriptVariant_ChangeType(&retvar, VT_STR);
-        ScriptVariant_ToString(svar, StrCache_Get(retvar.strVal));
-        ScriptVariant_ToString(rightChild, buf);
-        strcat(StrCache_Get(retvar.strVal), buf);
+        retvar.strVal = StrCache_Pop(len1 + len2);
+        dst = StrCache_Get(retvar.strVal);
+        ScriptVariant_ToString(svar, dst);
+        ScriptVariant_ToString(rightChild, dst + len1);
+        dst[len1 + len2] = '\0'; // shouldn't be needed, but just in case
     }
     else
     {

engine/openborscript.c
@@ -1224,13 +1224,13 @@ HRESULT openbor_getmodelproperty(ScriptVariant **varlist , ScriptVariant **pretv
     case 2:
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, model_cache[iArg].name);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(model_cache[iArg].name);
         break;
     }
     case 3:
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, model_cache[iArg].path);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(model_cache[iArg].path);
         break;
     }
     case 4:                                                    //Loaded?
@@ -2553,7 +2553,6 @@ int mapstrings_entityproperty(ScriptVariant **varlist, int paramCount)
 HRESULT openbor_getentityproperty(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount)
 {
     entity *ent			= NULL;
-    char *tempstr		= NULL;
     ScriptVariant *arg	= NULL;
     ScriptVariant *arg1	= NULL;
     s_sprite *spr;
@@ -3009,7 +3008,7 @@ HRESULT openbor_getentityproperty(ScriptVariant **varlist , ScriptVariant **pret
     case _ep_defaultname:
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, ent->defaultmodel->name);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(ent->defaultmodel->name);
         break;
     }
     case _ep_defense:
@@ -3910,7 +3909,7 @@ HRESULT openbor_getentityproperty(ScriptVariant **varlist , ScriptVariant **pret
     case _ep_model:
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, ent->model->name);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(ent->model->name);
         break;
     }
     case _ep_mp:
@@ -3946,7 +3945,7 @@ HRESULT openbor_getentityproperty(ScriptVariant **varlist , ScriptVariant **pret
     case _ep_name:
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, ent->name);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(ent->name);
         break;
     }
     case _ep_nextanim:
@@ -4081,9 +4080,7 @@ HRESULT openbor_getentityproperty(ScriptVariant **varlist , ScriptVariant **pret
     case _ep_path:
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        tempstr = ent->modeldata.path;
-
-        StrCache_Copy((*pretvar)->strVal, tempstr);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(ent->modeldata.path);
         break;
     }
     case _ep_pathfindstep:
@@ -4333,7 +4330,7 @@ HRESULT openbor_getentityproperty(ScriptVariant **varlist , ScriptVariant **pret
         case _ep_spritea_file:
         {
             ScriptVariant_ChangeType(*pretvar, VT_STR);
-            StrCache_Copy((*pretvar)->strVal, sprite_map[i].node->filename);
+            (*pretvar)->strVal = StrCache_CreateNewFrom(sprite_map[i].node->filename);
             break;
         }
         case _ep_spritea_offsetx:
@@ -6809,7 +6806,7 @@ HRESULT openbor_getplayerproperty(ScriptVariant **varlist , ScriptVariant **pret
     case _pp_name:
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, (char *)player[index].name);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(player[index].name);
         break;
     }
     case _pp_colourmap:
@@ -7902,6 +7899,7 @@ HRESULT openbor_getfilestreamline(ScriptVariant **varlist , ScriptVariant **pret
 {
     int length;
     char *buf;
+    char *dst;
     ScriptVariant *arg = NULL;
     LONG filestreamindex;
 
@@ -7926,7 +7924,10 @@ HRESULT openbor_getfilestreamline(ScriptVariant **varlist , ScriptVariant **pret
         ++length;
     }
 
-    StrCache_NCopy((*pretvar)->strVal, buf, length);
+    (*pretvar)->strVal = StrCache_Pop(length);
+    dst = StrCache_Get((*pretvar)->strVal);
+    memcpy(dst, buf, length);
+    dst[length] = '\0';
 
     return S_OK;
 }
@@ -7966,7 +7967,7 @@ HRESULT openbor_getfilestreamargument(ScriptVariant **varlist , ScriptVariant **
     if(stricmp(argtype, "string") == 0)
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, (char *)findarg(filestreams[filestreamindex].buf + filestreams[filestreamindex].pos, argument));
+        (*pretvar)->strVal = StrCache_CreateNewFrom(findarg(filestreams[filestreamindex].buf + filestreams[filestreamindex].pos, argument));
     }
     else if(stricmp(argtype, "int") == 0)
     {
@@ -9962,7 +9963,7 @@ HRESULT openbor_gettextobjproperty(ScriptVariant **varlist , ScriptVariant **pre
     case _top_text:
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, level->textobjs[ind].text);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(level->textobjs[ind].text);
         break;
     }
     case _top_time:
@@ -11299,7 +11300,7 @@ HRESULT openbor_key(ScriptVariant **varlist , ScriptVariant **pretvar, int param
     if(name)
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, name);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(name);
     }
     else
     {
@@ -13566,7 +13567,7 @@ HRESULT openbor_getsaveinfo(ScriptVariant **varlist , ScriptVariant **pretvar, i
     else if(0 == stricmp(prop, "name"))
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, slot->dName);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(slot->dName);
     }
     else if(0 == stricmp(prop, "playername"))
     {
@@ -13575,7 +13576,7 @@ HRESULT openbor_getsaveinfo(ScriptVariant **varlist , ScriptVariant **pretvar, i
             goto gsi_error;
         }
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, slot->pName[ltemp]);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(slot->pName[ltemp]);
     }
     else if(0 == stricmp(prop, "health"))
     {

engine/source/openborscript/level.c
@@ -187,7 +187,7 @@ HRESULT openbor_get_set_property(ScriptVariant **varlist, ScriptVariant **pretva
         case SET_PROP_NAME:
 
             ScriptVariant_ChangeType(*pretvar, VT_STR);
-            StrCache_Copy((*pretvar)->strVal, handle->name);
+            (*pretvar)->strVal = StrCache_CreateNewFrom(handle->name);
             break;
 
         case SET_PROP_PLAYER_MAX:
@@ -412,7 +412,7 @@ HRESULT openbor_get_level_property(ScriptVariant **varlist, ScriptVariant **pret
         case LEVEL_PROP_BOSS_MUSIC_NAME:
 
             ScriptVariant_ChangeType(*pretvar, VT_STR);
-            StrCache_Copy((*pretvar)->strVal, handle->bossmusic);
+            (*pretvar)->strVal = StrCache_CreateNewFrom(handle->bossmusic);
             break;
 
         case LEVEL_PROP_BOSS_MUSIC_OFFSET:
@@ -655,7 +655,7 @@ HRESULT openbor_get_level_property(ScriptVariant **varlist, ScriptVariant **pret
         case LEVEL_PROP_NAME:
 
             ScriptVariant_ChangeType(*pretvar, VT_STR);
-            StrCache_Copy((*pretvar)->strVal, handle->name);
+            (*pretvar)->strVal = StrCache_CreateNewFrom(handle->name);
             break;
 
         case LEVEL_PROP_PALETTE_BLENDING_COLLECTION:

engine/source/openborscript/string.c
@@ -30,7 +30,7 @@ HRESULT openbor_strinfirst(ScriptVariant **varlist , ScriptVariant **pretvar, in
     if (tempstr != NULL)
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, tempstr);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(tempstr);
     }
     else
     {
@@ -60,12 +60,13 @@ HRESULT openbor_strinlast(ScriptVariant **varlist , ScriptVariant **pretvar, int
         goto sil_error;
     }
 
+    // this definitely doesn't work??? it interprets a string cache index as a character
     tempstr = strrchr((char *)StrCache_Get(varlist[0]->strVal), varlist[1]->strVal);
 
     if (tempstr != NULL)
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, tempstr);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(tempstr);
     }
     else
     {
@@ -81,6 +82,10 @@ sil_error:
 //strleft(char string, int i);
 HRESULT openbor_strleft(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount)
 {
+    const char *src;
+    char *dst;
+    int srcLength, dstLength;
+
     if(paramCount < 2)
     {
         goto sl_error;
@@ -91,8 +96,15 @@ HRESULT openbor_strleft(ScriptVariant **varlist , ScriptVariant **pretvar, int p
         printf("\n Error, strleft({string}, {characters}): Invalid or missing parameter. Strleft must be passed valid {string} and number of {characters}.\n");
         goto sl_error;
     }
+
+    src = StrCache_Get(varlist[0]->strVal);
+    srcLength = strlen(src);
+    dstLength = (srcLength < varlist[1]->lVal) ? srcLength : varlist[1]->lVal;
     ScriptVariant_ChangeType(*pretvar, VT_STR);
-    StrCache_NCopy((*pretvar)->strVal, (char *)StrCache_Get(varlist[0]->strVal), varlist[1]->lVal);
+    (*pretvar)->strVal = StrCache_Pop(dstLength);
+    dst = StrCache_Get((*pretvar)->strVal);
+    memcpy(dst, src, dstLength);
+    dst[dstLength] = '\0';
 
     return S_OK;
 sl_error:
@@ -159,7 +171,7 @@ HRESULT openbor_strright(ScriptVariant **varlist , ScriptVariant **pretvar, int 
     if (tempstr && tempstr[0])
     {
         ScriptVariant_ChangeType(*pretvar, VT_STR);
-        StrCache_Copy((*pretvar)->strVal, &tempstr[varlist[1]->lVal]);
+        (*pretvar)->strVal = StrCache_CreateNewFrom(&tempstr[varlist[1]->lVal]);
     }
     else
     {

engine/source/scriptlib/ScriptVariant.h
@@ -38,6 +38,7 @@ void StrCache_Clear();
 //void StrCache_Init();
 void StrCache_Collect(int index);
 int StrCache_Pop();
+int StrCache_CreateNewFrom(const CHAR *str);
 CHAR *StrCache_Get(int index);
 void StrCache_Copy(int index, CHAR *str);
 void StrCache_NCopy(int index, CHAR *str, int n);

commit 082c3db4f99f04edcf97d5882fa4b75fd847c843

Fix reference counting bug introduced by b6e3f4131
This is the fix for the underlying cause of the NULL dereference mentioned
in 9e690e6fe. There was a case where we were calling StrCache_Collect() on
an uninitialized string variant. Add asserts to guard against this in the
future, and initialize vt->strVal to an invalid value in
ScriptVariant_ChangeType() since 0 is a valid string cache index.

engine/openbor.c
@@ -1251,9 +1251,9 @@ int getsyspropertybyindex(ScriptVariant *var, int index)
         var->strVal = StrCache_CreateNewFrom(branch_name);
         break;
     case _sv_current_branch:
-        ScriptVariant_ChangeType(var, VT_STR);
         if(level != NULL && levelsets && levelsets[current_set].levelorder && levelsets[current_set].levelorder[current_level].branchname)
         {
+            ScriptVariant_ChangeType(var, VT_STR);
             var->strVal = StrCache_CreateNewFrom(levelsets[current_set].levelorder[current_level].branchname);
         }
         else

engine/source/scriptlib/ScriptVariant.c
@@ -6,10 +6,11 @@
  * Copyright (c) 2004 - 2014 OpenBOR Team
  */
 
-#include "ScriptVariant.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "globals.h"
+#include "ScriptVariant.h"
 
 typedef struct
 {
@@ -33,7 +34,10 @@ void StrCache_Clear()
     {
         for(i = 0; i < strcache_size; i++)
         {
-            if(strcache[i].str) free(strcache[i].str);
+            if(strcache[i].str)
+            {
+                free(strcache[i].str);
+            }
             strcache[i].str = NULL;
         }
         free(strcache);
@@ -57,7 +61,6 @@ void StrCache_Init()
     strcache_index = malloc(sizeof(*strcache_index) * STRCACHE_INC);
     for(i = 0; i < STRCACHE_INC; i++)
     {
-        if (strcache[i].str) free(strcache[i].str);
         strcache[i].str = NULL;
         strcache_index[i] = i;
     }
@@ -76,6 +79,9 @@ void StrCache_Resize(int index, int size)
 
 void StrCache_Collect(int index)
 {
+    assert(index >= 0);
+    assert(strcache[index].ref > 0);
+
     strcache[index].ref--;
     //assert(strcache[index].ref>=0);
     if(!strcache[index].ref)
@@ -83,7 +89,7 @@ void StrCache_Collect(int index)
         //if(strcache[index].len > MAX_STR_VAR_LEN)
         //	StrCache_Resize(index, MAX_STR_VAR_LEN);
         //assert(strcache_top+1<strcache_size);
-        if (strcache[index].str) free(strcache[index].str);
+        free(strcache[index].str);
         strcache[index].str = NULL;
         strcache_index[++strcache_top] = index;
     }
@@ -104,6 +110,8 @@ int StrCache_Pop(int length)
         {
             strcache_index[i] = strcache_size + i;
         }
+
+        memset(strcache + strcache_size, 0, sizeof(*strcache) * STRCACHE_INC);
 
         //printf("debug: dumping string cache....\n");
         //for(i=0; i<strcache_size; i++)
@@ -156,18 +164,22 @@ void StrCache_NCopy(int index, CHAR *str, int n)
 
 CHAR *StrCache_Get(int index)
 {
+    assert(index >= 0);
     //assert(index<strcache_size);
     return strcache[index].str;
 }
 
 int StrCache_Len(int index)
 {
+    assert(index >= 0);
     //assert(index<strcache_size);
     return strcache[index].len;
 }
 
 void StrCache_Grab(int index)
 {
+    assert(index >= 0);
+    assert(strcache[index].ref > 0);
     ++strcache[index].ref;
 }
 
@@ -196,6 +208,10 @@ void ScriptVariant_ChangeType(ScriptVariant *var, VARTYPE cvt)
         StrCache_Collect(var->strVal);
     }
     var->vt = cvt;
+    if (cvt == VT_STR)
+    {
+        var->strVal = -1;
+    }
 }
 
 // find an existing constant before copy
@@ -206,7 +222,7 @@ void ScriptVariant_ParseStringConstant(ScriptVariant *var, CHAR *str)
     int i;
     for(i = 0; i < strcache_size; i++)
     {
-        if(strcache[i].ref && strcache[i].str && strcmp(str, strcache[i].str) == 0)
+        if (strcache[i].ref && strcmp(str, strcache[i].str) == 0)
         {
             var->strVal = i;
             strcache[i].ref++;