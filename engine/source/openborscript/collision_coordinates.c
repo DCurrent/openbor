00a65f563f5a4f303f68a679775058fa5fd6f677
Damon V. Caskey <dc@caskeys.com>
2018-01-05 14:34:20 -0500

    Collision Coordinates Access
    Collision coordinate access and mutator functions should be completed.

--- engine/source/openborscript/collision_coordinates.c
+++ engine/source/openborscript/collision_coordinates.c
@@ -7,7 +7,7 @@
  */
 
 // Coordinate Properties
-// 2019-01-03
+// 2018-01-03
 // Caskey, Damon V.
 //
 // Access to body coordinate properties by handle.
@@ -109,10 +109,10 @@ HRESULT openbor_get_collision_coordinates_property(ScriptVariant **varlist, Scri
     #undef ARG_PROPERTY
 }
 
-// set_coordinate_property(void handle, int property, value)
+// set_collision_coordinates_property(void handle, int property, value)
 HRESULT openbor_set_collision_coordinates_property(ScriptVariant **varlist, ScriptVariant **pretvar, int paramCount)
 {
-    #define SELF_NAME           "set_coordinate_property(void handle, int property, value)"
+    #define SELF_NAME           "set_collision_coordinates_property(void handle, int property, value)"
     #define ARG_MINIMUM         3   // Minimum required arguments.
     #define ARG_HANDLE          0   // Handle (pointer to property structure).
     #define ARG_PROPERTY        1   // Property to access.
@@ -126,10 +126,9 @@ HRESULT openbor_set_collision_coordinates_property(ScriptVariant **varlist, Scri
     // Value carriers to apply on properties after
     // taken from argument.
     int         temp_int;
-    //s_defense   *temp_defense;
 
     // Verify incoming arguments. There must be a
-    // pointer for the animation handle, an integer
+    // pointer for the property handle, an integer
     // property, and a new value to apply.
     if(paramCount < ARG_MINIMUM
        || varlist[ARG_HANDLE]->vt != VT_PTR
@@ -156,59 +155,43 @@ HRESULT openbor_set_collision_coordinates_property(ScriptVariant **varlist, Scri
 
             break;
 
-        case BODY_COLLISION_PROP_POSITION_X:
+        case COLLISION_COORDINATES_PROP_DEPTH_FOREGROUND:
 
             if(SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
             {
-                //handle->coords.x = temp_int;
+                handle->z2 = temp_int;
             }
             break;
 
-        case BODY_COLLISION_PROP_POSISTION_Y:
+        case COLLISION_COORDINATES_PROP_HEIGHT:
 
             if(SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
             {
-                //handle->coords.y = temp_int;
+                handle->height = temp_int;
             }
             break;
 
-        case BODY_COLLISION_PROP_SIZE_X:
+        case COLLISION_COORDINATES_PROP_WIDTH:
 
             if(SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
             {
-                //handle->coords.width = temp_int;
+                handle->width = temp_int;
             }
             break;
 
-        case BODY_COLLISION_PROP_SIZE_Y:
+        case COLLISION_COORDINATES_PROP_X:
 
             if(SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
             {
-                //handle->coords.height = temp_int;
+                handle->x = temp_int;
             }
             break;
 
-        case BODY_COLLISION_PROP_SIZE_Z_1:
+        case COLLISION_COORDINATES_PROP_Y:
 
             if(SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
             {
-                //handle->coords.z1 = temp_int;
-            }
-            break;
-
-        case BODY_COLLISION_PROP_SIZE_Z_2:
-
-            if(SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
-            {
-                //handle->coords.z2 = temp_int;
-            }
-            break;
-
-        case BODY_COLLISION_PROP_TAG:
-
-            if(SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
-            {
-                handle->tag = temp_int;
+                handle->y = temp_int;
             }
             break;
 
