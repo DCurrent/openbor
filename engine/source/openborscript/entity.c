39b2a596755857c2d478f408ec53b1565664e6da
Damon V. Caskey <dc@caskeys.com>
2018-04-02 15:37:11 -0400

    Begin coding entity mapstrings.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -20,3 +20,45 @@ typedef enum
     _entity_the_end,
 
 } e_entity_properties;
+
+// Use string property argument to find an
+// integer property constant and populate
+// varlist->lval.
+int mapstrings_entity_property(ScriptVariant **varlist, int paramCount)
+{
+    #define ARG_MINIMUM     2   // Minimum number of arguments allowed in varlist.
+    #define ARG_PROPERTY    1   // Varlist element carrying which property is requested.
+
+    char *propname = NULL;  // Placeholder for string property name from varlist.
+    int prop;               // Placeholder for integer constant located by string.
+
+    static const char *proplist[] =
+    {
+        "position_alternate_base",
+        "position_base",
+        "position_x",
+        "position_y",
+        "position_z",
+    };
+
+    // If the minimum argument count
+    // was not passed, then there is
+    // nothing to map. Return true - we'll
+    // catch the mistake in property access
+    // functions.
+    if(paramCount < ARG_MINIMUM)
+    {
+        return 1;
+    }
+
+    // See macro - will return 0 on fail.
+    MAPSTRINGS(varlist[ARG_PROPERTY], proplist, _entity_the_end,
+               "Property name '%s' is not supported by entities.\n");
+
+
+    // If we made it this far everything should be OK.
+    return 1;
+
+    #undef ARG_MINIMUM
+    #undef ARG_PROPERTY
+}
