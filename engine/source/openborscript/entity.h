7cf81f9840106ebfaa2cef46e215dd7e6084d217
Damon V. Caskey <dc@caskeys.com>
2018-04-02 11:25:18 -0400

    Add basic metainfo.

---
+++ engine/source/openborscript/entity.h
@@ -0,0 +1,11 @@
+/*
+ * OpenBOR - http://www.chronocrash.com
+ * -----------------------------------------------------------------------
+ * All rights reserved. See LICENSE in OpenBOR root for license details.
+ *
+ * Copyright (c) 2004 - 2018 OpenBOR Team
+ */
+
+// Entity Properties
+// 2018-04-02
+// Caskey, Damon V.
