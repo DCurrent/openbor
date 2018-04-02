21b0f4abe3f00cffb98df176403684933025c2c2
Damon V. Caskey <dc@caskeys.com>
2018-04-02 15:24:57 -0400

    Enum position propertes.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -9,3 +9,14 @@
 // Entity Properties
 // 2018-04-02
 // Caskey, Damon V.
+
+typedef enum
+{
+    _entity_position_alternate_base,
+    _entity_position_base,
+    _entity_position_x,
+    _entity_position_y,
+    _entity_position_z,
+    _entity_the_end,
+
+} e_entity_properties;
