ecf55b933a4fe3938e00b35268dc25cf705a08cf
Damon V. Caskey <dc@caskeys.com>
2018-04-03 10:19:39 -0400

    Arrow on constant.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -16,6 +16,7 @@ typedef enum
     _entity_animation_animation,
     _entity_animation_collection,
     _entity_animation_frame,
+    _entity_arrow_on,
     _entity_position_alternate_base,
     _entity_position_base,
     _entity_position_direction,
