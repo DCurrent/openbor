06826b2c080457bd6e5f75740f2aef87b7f79024
Damon V. Caskey <dc@caskeys.com>
2018-04-03 10:21:05 -0400

    Get arrow on.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -154,6 +154,13 @@ HRESULT openbor_get_entity_property(ScriptVariant **varlist , ScriptVariant **pr
 
             break;
 
+        case _entity_arrow_on:
+
+            ScriptVariant_ChangeType(*pretvar, VT_INTEGER);
+            (*pretvar)->lVal = (LONG)handle->arrowon;
+
+            break;
+
         case _entity_position_alternate_base:
 
             ScriptVariant_ChangeType(*pretvar, VT_DECIMAL);
