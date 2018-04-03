ccd8a0c1fffcbf8dff2c0cf734b8fcd1d23eca5b
Damon V. Caskey <dc@caskeys.com>
2018-04-03 11:29:13 -0400

    Move entity property enum to entity.h

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -6,28 +6,6 @@
  * Copyright (c) 2004 - 2018 OpenBOR Team
  */
 
-// Entity Properties
-// 2018-04-02
-// Caskey, Damon V.
-
-typedef enum
-{
-    _entity_animation_animating,
-    _entity_animation_animation,
-    _entity_animation_collection,
-    _entity_animation_frame,
-    _entity_arrow_on,
-    _entity_attacking,
-    _entity_position_alternate_base,
-    _entity_position_base,
-    _entity_position_direction,
-    _entity_position_x,
-    _entity_position_y,
-    _entity_position_z,
-    _entity_the_end,
-
-} e_entity_properties;
-
 // Use string property argument to find an
 // integer property constant and populate
 // varlist->lval.
--- engine/source/openborscript/entity.h
+++ engine/source/openborscript/entity.h
@@ -10,6 +10,24 @@
 // 2018-04-02
 // Caskey, Damon V.
 
+typedef enum
+{
+    _entity_animation_animating,
+    _entity_animation_animation,
+    _entity_animation_collection,
+    _entity_animation_frame,
+    _entity_arrow_on,
+    _entity_attacking,
+    _entity_position_alternate_base,
+    _entity_position_base,
+    _entity_position_direction,
+    _entity_position_x,
+    _entity_position_y,
+    _entity_position_z,
+    _entity_the_end,
+
+} e_entity_properties;
+
 HRESULT openbor_get_entity_property(ScriptVariant **varlist, ScriptVariant **pretvar, int paramCount);
 HRESULT openbor_set_entity_property(ScriptVariant **varlist, ScriptVariant **pretvar, int paramCount);
 
