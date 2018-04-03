4741c8646e8ac9cf1c58ac60216e3c452f5b2fd0
Damon V. Caskey <dc@caskeys.com>
2018-04-03 16:01:57 -0400

    Constant binding.

--- engine/source/openborscript/entity.h
+++ engine/source/openborscript/entity.h
@@ -21,6 +21,7 @@ typedef enum
     _entity_attack_id_incoming,
     _entity_attack_id_outgoing,
     _entity_autokill,
+    _entity_binding,
     _entity_position_alternate_base,
     _entity_position_base,
     _entity_position_direction,
