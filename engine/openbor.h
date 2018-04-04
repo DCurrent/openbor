940996423b7a2a064755468ad5373da23b8d8e0e
Damon V. Caskey <dc@caskeys.com>
2018-04-04 14:19:13 -0400

    Rename hy_by_attack_id to attack_id_incoming.

--- engine/openbor.h
+++ engine/openbor.h
@@ -2238,7 +2238,7 @@ typedef struct entity
     int (*takedamage)(struct entity *, s_collision_attack *, int);
     int (*trymove)(float, float);
     int attack_id_outgoing;
-    int hit_by_attack_id;
+    int attack_id_incoming;
     int hitwall; // == 1 in the instant that hit the wall/platform/obstacle, else == 0
     unsigned char *colourmap;
     //struct entity   *thrower;
