1b028b7b04d9b2c026aa04539b2d57ce6ece250c
Damon V. Caskey <dc@caskeys.com>
2018-04-04 13:41:19 -0400

    Get blink.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -181,6 +181,13 @@ HRESULT openbor_get_entity_property(ScriptVariant **varlist , ScriptVariant **pr
 
             break;
 
+        case _entity_blink:
+
+            ScriptVariant_ChangeType(*pretvar, VT_INTEGER);
+            (*pretvar)->lVal = (LONG)handle->blink;
+
+            break;
+
         case _entity_position_alternate_base:
 
             ScriptVariant_ChangeType(*pretvar, VT_DECIMAL);
