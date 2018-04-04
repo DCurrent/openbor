3b37b1dcb7609913d669e9829cbcd5cbacbff199
Damon V. Caskey <dc@caskeys.com>
2018-04-04 14:33:40 -0400

    Get boomerang_loop.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -197,6 +197,13 @@ HRESULT openbor_get_entity_property(ScriptVariant **varlist , ScriptVariant **pr
 
             break;
 
+        case _entity_boomerang_loop:
+
+            ScriptVariant_ChangeType(*pretvar, VT_INTEGER);
+            (*pretvar)->lVal = (LONG)handle->boomerang_loop;
+
+            break;
+
         case _entity_position_alternate_base:
 
             ScriptVariant_ChangeType(*pretvar, VT_DECIMAL);
