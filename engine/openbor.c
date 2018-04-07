c49c2729099e172524d0fe8556482a8571f3344a
Damon V. Caskey <dc@caskeys.com>
2018-04-07 12:23:37 -0400

    Use ent argument in place of self in boomerang_catch().

--- engine/openbor.c
+++ engine/openbor.c
@@ -26913,34 +26913,34 @@ void sort_invert(entity *entity)
 // Triggers a catch animation while destroying
 // boomerang projectile if the boomerang is returning
 // and within range of catch animation.
-int boomerang_catch(entity *entity, float distance_x_current)
+int boomerang_catch(entity *ent, float distance_x_current)
 {
-    if( (self->position.x >= self->parent->position.x && self->parent->direction == DIRECTION_RIGHT && self->velocity.x <= 0) ||
-                (self->position.x <= self->parent->position.x && self->parent->direction == DIRECTION_LEFT  && self->velocity.x >= 0) )
+    if( (ent->position.x >= ent->parent->position.x && ent->parent->direction == DIRECTION_RIGHT && ent->velocity.x <= 0) ||
+                (ent->position.x <= ent->parent->position.x && ent->parent->direction == DIRECTION_LEFT  && ent->velocity.x >= 0) )
     {
-       if(!self->parent->inpain && !self->parent->falling && !self->parent->dead && self->boomerang_loop > 1 )
+       if(!ent->parent->inpain && !ent->parent->falling && !ent->parent->dead && ent->boomerang_loop > 1 )
        {
-            if(self->parent->position.y <= self->parent->base && validanim(self->parent, ANI_GETBOOMERANG))
+            if(ent->parent->position.y <= ent->parent->base && validanim(ent->parent, ANI_GETBOOMERANG))
             {
-                if( distance_x_current >= self->parent->modeldata.animation[ANI_GETBOOMERANG]->range.min.x && distance_x_current <= self->parent->modeldata.animation[ANI_GETBOOMERANG]->range.max.x )
+                if( distance_x_current >= ent->parent->modeldata.animation[ANI_GETBOOMERANG]->range.min.x && distance_x_current <= ent->parent->modeldata.animation[ANI_GETBOOMERANG]->range.max.x )
                 {
-                    self->parent->takeaction = common_animation_normal;
-                    self->parent->attacking = 0;
-                    self->parent->idling = 0;
-                    ent_set_anim(self->parent, ANI_GETBOOMERANG, 0);
-                    kill(self);
+                    ent->parent->takeaction = common_animation_normal;
+                    ent->parent->attacking = 0;
+                    ent->parent->idling = 0;
+                    ent_set_anim(ent->parent, ANI_GETBOOMERANG, 0);
+                    kill(ent);
                     return 1;
                 }
             }
-            else if(inair(self->parent) && validanim(self->parent, ANI_GETBOOMERANGINAIR))
+            else if(inair(ent->parent) && validanim(ent->parent, ANI_GETBOOMERANGINAIR))
             {
-                if( distance_x_current >= self->parent->modeldata.animation[ANI_GETBOOMERANGINAIR]->range.min.x && distance_x_current <= self->parent->modeldata.animation[ANI_GETBOOMERANGINAIR]->range.max.x )
+                if( distance_x_current >= ent->parent->modeldata.animation[ANI_GETBOOMERANGINAIR]->range.min.x && distance_x_current <= ent->parent->modeldata.animation[ANI_GETBOOMERANGINAIR]->range.max.x )
                 {
-                    self->parent->takeaction = common_animation_normal;
-                    self->parent->attacking = 0;
-                    self->parent->idling = 0;
-                    ent_set_anim(self->parent, ANI_GETBOOMERANGINAIR, 0);
-                    kill(self);
+                    ent->parent->takeaction = common_animation_normal;
+                    ent->parent->attacking = 0;
+                    ent->parent->idling = 0;
+                    ent_set_anim(ent->parent, ANI_GETBOOMERANGINAIR, 0);
+                    kill(ent);
                     return 1;
                 }
             }
