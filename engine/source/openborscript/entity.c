93f1ca95fdc1a96a1dabc65fc55f50da6b206f59
Damon V. Caskey <dc@caskeys.com>
2018-04-08 18:12:35 -0400

    Get colorset_dying_2 property.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -237,6 +237,13 @@ HRESULT openbor_get_entity_property(ScriptVariant **varlist , ScriptVariant **pr
 
             break;
 
+        case _entity_colorset_dying_2:
+
+            ScriptVariant_ChangeType(*pretvar, VT_INTEGER);
+            (*pretvar)->lVal = (LONG)handle->dying2;
+
+            break;
+
         case _entity_position_alternate_base:
 
             ScriptVariant_ChangeType(*pretvar, VT_DECIMAL);
