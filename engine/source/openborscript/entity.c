cd92d63cfd983f39120caa3d6b0bcf973cf3917c
Damon V. Caskey <dc@caskeys.com>
2018-04-08 10:23:31 -0400

    Set boss property.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -434,6 +434,15 @@ HRESULT openbor_set_entity_property(ScriptVariant **varlist, ScriptVariant **pre
 
             break;
 
+        case _entity_boss:
+
+            if(SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
+            {
+                handle->boss = temp_int;
+            }
+
+            break;
+
         case _entity_position_alternate_base:
 
             if(SUCCEEDED(ScriptVariant_DecimalValue(varlist[ARG_VALUE], &temp_float)))
