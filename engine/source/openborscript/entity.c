8f95343b7a34d636b5b263d47ddbaf36056dffa5
Damon V. Caskey <dc@caskeys.com>
2018-04-09 09:40:38 -0400

    Set combo_time property.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -564,6 +564,15 @@ HRESULT openbor_set_entity_property(ScriptVariant **varlist, ScriptVariant **pre
 
             break;
 
+        case _entity_combo_time:
+
+            if(SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
+            {
+                handle->combotime = temp_int;
+            }
+
+            break;
+
         case _entity_position_alternate_base:
 
             if(SUCCEEDED(ScriptVariant_DecimalValue(varlist[ARG_VALUE], &temp_float)))
