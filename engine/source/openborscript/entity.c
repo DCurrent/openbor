d5585f5fcdbc19c9c74bf0fc6f0f0f45f71162de
Damon V. Caskey <dc@caskeys.com>
2018-04-09 09:39:37 -0400

    Get combo_time property.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -269,6 +269,13 @@ HRESULT openbor_get_entity_property(ScriptVariant **varlist , ScriptVariant **pr
 
             break;
 
+        case _entity_combo_time:
+
+            ScriptVariant_ChangeType(*pretvar, VT_INTEGER);
+            (*pretvar)->lVal = (LONG)handle->combotime;
+
+            break;
+
         case _entity_position_alternate_base:
 
             ScriptVariant_ChangeType(*pretvar, VT_DECIMAL);
