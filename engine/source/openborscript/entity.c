803f64b3a69abd831f3701e43263226c673594de
Damon V. Caskey <dc@caskeys.com>
2018-04-11 14:37:28 -0400

    Set damage_on_landing property.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -601,6 +601,12 @@ HRESULT openbor_set_entity_property(ScriptVariant **varlist, ScriptVariant **pre
 
             break;
 
+        case _entity_damage_on_landing:
+
+            // Read only.
+
+            break;
+
         case _entity_position_alternate_base:
 
             if(SUCCEEDED(ScriptVariant_DecimalValue(varlist[ARG_VALUE], &temp_float)))
