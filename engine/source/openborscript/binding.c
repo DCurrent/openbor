f7484828883cb901ed2abd2cb2188c12f9b6ff2c
Damon V. Caskey <dc@caskeys.com>
2018-04-16 08:03:27 -0400

    Move binding property enum to header.

--- engine/source/openborscript/binding.c
+++ engine/source/openborscript/binding.c
@@ -12,19 +12,6 @@
 
 #include "scriptcommon.h"
 
-typedef enum
-{
-    _binding_animation,
-    _binding_bind_x,
-    _binding_bind_y,
-    _binding_bind_z,
-    _binding_direction,
-    _binding_entity,
-    _binding_offset,
-    _binding_sort_id,
-    _binding_the_end,
-} e_binding_properties;
-
 // Use string property argument to find an
 // integer property constant and populate
 // varlist->lval.
--- engine/source/openborscript/binding.h
+++ engine/source/openborscript/binding.h
@@ -1,7 +1,20 @@
 
+typedef enum
+{
+    _binding_animation,
+    _binding_bind_x,
+    _binding_bind_y,
+    _binding_bind_z,
+    _binding_direction,
+    _binding_entity,
+    _binding_offset,
+    _binding_sort_id,
+    _binding_the_end,
+} e_binding_properties;
 
 // Binding properties.
 HRESULT openbor_get_binding_property(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
 HRESULT openbor_set_binding_property(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount);
 
 int mapstrings_binding(ScriptVariant **varlist, int paramCount);
+
