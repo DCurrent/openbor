53c7e9c73ff0a8eae8ca72efca5d46b5a4118c2a
Caskey, Damon V <dc@caskeys.com>
2018-05-15 13:10:25 -0400

    Get and set axis lateral int.

--- engine/source/openborscript/axis.c
+++ engine/source/openborscript/axis.c
@@ -299,3 +299,173 @@ HRESULT openbor_set_axis_plane_lateral_float_property(ScriptVariant **varlist, S
     #undef ARG_PROPERTY
     #undef ARG_VALUE
 }
+
+// Caskey, Damon  V.
+// 2018-05-14
+//
+// Return an axis property. Requires
+// the handle from axis property
+// and property name to access.
+HRESULT openbor_get_axis_plane_lateral_int_property(ScriptVariant **varlist , ScriptVariant **pretvar, int paramCount)
+{
+    #define SELF_NAME       "openbor_get_axis_plane_lateral_int_property(void handle, char property)"
+    #define ARG_MINIMUM     2   // Minimum required arguments.
+    #define ARG_HANDLE      0   // Handle (pointer to property structure).
+    #define ARG_PROPERTY    1   // Property to access.
+
+    s_axis_plane_lateral_int        *handle     = NULL; // Property handle.
+    e_axis_plane_lateral_properties property    = 0;    // Property argument.
+
+    // Clear pass by reference argument used to send
+    // property data back to calling script.     .
+    ScriptVariant_Clear(*pretvar);
+
+    // Map string property name to a
+    // matching integer constant.
+    mapstrings_axis_plane_lateral_property(varlist, paramCount);
+
+    // Verify arguments. There should at least
+    // be a pointer for the property handle and an integer
+    // to determine which property constant is accessed.
+    if(paramCount < ARG_MINIMUM
+       || varlist[ARG_HANDLE]->vt != VT_PTR
+       || varlist[ARG_PROPERTY]->vt != VT_INTEGER)
+    {
+        *pretvar = NULL;
+        goto error_local;
+    }
+    else
+    {
+        // Populate local vars for readability.
+        handle      = (s_axis_plane_lateral_int *)varlist[ARG_HANDLE]->ptrVal;
+        property    = (LONG)varlist[ARG_PROPERTY]->lVal;
+    }
+
+    // All values are float (DOUBLE) type.
+    ScriptVariant_ChangeType(*pretvar, VT_INTEGER);
+
+    switch(property)
+    {
+        case _AXIS_PLANE_LATERAL_X:
+
+            (*pretvar)->lVal = (LONG)handle->x;
+
+            break;
+
+        case _AXIS_PLANE_LATERAL_Z:
+
+            (*pretvar)->lVal = (LONG)handle->z;
+
+            break;
+
+        default:
+
+            printf("Unsupported property.\n");
+            goto error_local;
+
+            break;
+    }
+
+    return S_OK;
+
+    error_local:
+
+    printf("You must provide a valid handle and property name: " SELF_NAME "\n");
+    *pretvar = NULL;
+
+    return E_FAIL;
+
+    #undef SELF_NAME
+    #undef ARG_MINIMUM
+    #undef ARG_HANDLE
+    #undef ARG_INDEX
+}
+
+// Caskey, Damon  V.
+// 2018-05-14
+//
+// Mutate an axis property. Requires
+// the handle from axis property, property
+// name to modify, and the new value.
+HRESULT openbor_set_axis_plane_lateral_int_property(ScriptVariant **varlist, ScriptVariant **pretvar, int paramCount)
+{
+    #define SELF_NAME           "openbor_set_axis_plane_lateral_float_property(void handle, char property, value)"
+    #define ARG_MINIMUM         3   // Minimum required arguments.
+    #define ARG_HANDLE          0   // Handle (pointer to property structure).
+    #define ARG_PROPERTY        1   // Property to access.
+    #define ARG_VALUE           2   // New value to apply.
+
+    int                             result      = S_OK; // Success or error?
+    s_axis_plane_lateral_int        *handle     = NULL; // Property handle.
+    e_axis_plane_lateral_properties property    = 0;    // Property to access.
+
+    // Value carriers to apply on properties after
+    // taken from argument.
+    int temp_int;
+
+    // Map string property name to a
+    // matching integer constant.
+    mapstrings_axis_plane_lateral_property(varlist, paramCount);
+
+    // Verify incoming arguments. There should at least
+    // be a pointer for the property handle and an integer
+    // to determine which property is accessed.
+    if(paramCount < ARG_MINIMUM
+       || varlist[ARG_HANDLE]->vt != VT_PTR
+       || varlist[ARG_PROPERTY]->vt != VT_INTEGER)
+    {
+        *pretvar = NULL;
+        goto error_local;
+    }
+
+    // Populate local handle and property vars.
+    handle      = (s_axis_plane_lateral_int *)varlist[ARG_HANDLE]->ptrVal;
+    property    = (LONG)varlist[ARG_PROPERTY]->lVal;
+
+    // All values are same type for this property set,
+    // so we can copy to temp var right here.
+    if(!SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
+    {
+        goto error_local;
+    }
+
+    // Which property to modify?
+    switch(property)
+    {
+
+        case _AXIS_PLANE_LATERAL_X:
+
+            handle->x = temp_int;
+
+            break;
+
+        case _AXIS_PLANE_LATERAL_Z:
+
+            handle->z = temp_int;
+
+            break;
+
+        default:
+
+            printf("Unsupported property.\n");
+            goto error_local;
+
+            break;
+    }
+
+    return result;
+
+    // Error trapping.
+    error_local:
+
+    printf("You must provide a valid binding handle, property, and new value: " SELF_NAME "\n");
+
+    result = E_FAIL;
+    return result;
+
+    #undef SELF_NAME
+    #undef ARG_MINIMUM
+    #undef ARG_HANDLE
+    #undef ARG_PROPERTY
+    #undef ARG_VALUE
+}
