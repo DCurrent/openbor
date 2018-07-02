37f504a5645552b24d0b3c21c0fc424455137b7f
whitedragon0000 <000whitedragon000@gmail.com>
2018-07-02 23:18:52 +0200

    update SDLActivity according to SDL2 2.0.8 (Jun 14)

--- engine/android/src/org/libsdl/app/SDLActivity.java
+++ engine/android/src/org/libsdl/app/SDLActivity.java
@@ -12,8 +12,10 @@
 
 package org.libsdl.app;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Hashtable;
 import java.lang.reflect.Method;
 
 import android.app.*;
@@ -41,6 +43,9 @@ import android.content.pm.PackageManager;
 import android.content.pm.ApplicationInfo;
 
 //Added imports
+import java.io.OutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import android.os.PowerManager.*;
 import android.content.res.*;
 //msmalik681 added imports for new pak copy!
@@ -96,13 +101,29 @@ public class SDLActivity extends Activity {
     protected static SDLClipboardHandler mClipboardHandler;
     protected static Hashtable<Integer, Object> mCursors;
     protected static int mLastCursorID;
+    protected static SDLGenericMotionListener_API12 mMotionListener;
 
     //White Dragon: added statics
     protected static WakeLock wakeLock;
     protected static View decorView;
-
+	
     // This is what SDL runs in. It invokes SDL_main(), eventually
     protected static Thread mSDLThread;
+
+    protected static SDLGenericMotionListener_API12 getMotionListener() {
+        if (mMotionListener == null) {
+            if (Build.VERSION.SDK_INT >= 26) {
+                mMotionListener = new SDLGenericMotionListener_API26();
+            } else
+            if (Build.VERSION.SDK_INT >= 24) {
+                mMotionListener = new SDLGenericMotionListener_API24();
+            } else {
+                mMotionListener = new SDLGenericMotionListener_API12();
+            }
+        }
+
+        return mMotionListener;
+    }
 
     /**
      * This method returns the name of the shared object with the application entry point
@@ -255,6 +276,7 @@ public class SDLActivity extends Activity {
 
         setWindowStyle(false);
 
+        //uTunnels copy pak
         CopyPak();
 
         //White Dragon: hide navigation bar programmatically
@@ -449,6 +471,7 @@ public class SDLActivity extends Activity {
         SDLActivity.mHasFocus = hasFocus;
         if (hasFocus) {
            mNextNativeState = NativeState.RESUMED;
+           SDLActivity.getMotionListener().reclaimRelativeMouseModeIfNeeded();
            hideSystemUI(); //White Dragon: disable navigation bar
         } else {
            mNextNativeState = NativeState.PAUSED;
@@ -592,7 +615,10 @@ public class SDLActivity extends Activity {
     /* The native thread has finished */
     public static void handleNativeExit() {
         SDLActivity.mSDLThread = null;
-        mSingleton.finish();
+
+        // Make sure we currently have a singleton before we try to call it.
+        if (mSingleton != null)
+            mSingleton.finish();
     }
 
 
@@ -642,18 +668,16 @@ public class SDLActivity extends Activity {
                     // This version of Android doesn't support the immersive fullscreen mode
                     break;
                 }
-/* This needs more testing, per bug 4096 - Enabling fullscreen on Android causes the app to toggle fullscreen mode continuously in a loop
- ***
                 if (context instanceof Activity) {
                     Window window = ((Activity) context).getWindow();
                     if (window != null) {
                         if ((msg.obj instanceof Integer) && (((Integer) msg.obj).intValue() != 0)) {
-                            int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            int flags = View.SYSTEM_UI_FLAG_FULLSCREEN |
                                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                                        View.SYSTEM_UI_FLAG_FULLSCREEN |
-                                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+					View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
                             window.getDecorView().setSystemUiVisibility(flags);
                             window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
                         } else {
@@ -665,7 +689,6 @@ public class SDLActivity extends Activity {
                 } else {
                     Log.e(TAG, "error handling message, getContext() returned no Activity");
                 }
-***/
                 break;
             case COMMAND_TEXTEDIT_HIDE:
                 if (mTextEdit != null) {
@@ -721,11 +744,11 @@ public class SDLActivity extends Activity {
     public static native void nativePause();
     public static native void nativeResume();
     public static native void onNativeDropFile(String filename);
-    public static native void onNativeResize(int x, int y, int format, float rate);
+    public static native void onNativeResize(int surfaceWidth, int surfaceHeight, int deviceWidth, int deviceHeight, int format, float rate);
     public static native void onNativeKeyDown(int keycode);
     public static native void onNativeKeyUp(int keycode);
     public static native void onNativeKeyboardFocusLost();
-    public static native void onNativeMouse(int button, int action, float x, float y);
+    public static native void onNativeMouse(int button, int action, float x, float y, boolean relative);
     public static native void onNativeTouch(int touchDevId, int pointerFingerId,
                                             int action, float x,
                                             float y, float p);
@@ -804,7 +827,6 @@ public class SDLActivity extends Activity {
         }
     }
 
-
     /**
      * This method is called by SDL using JNI.
      */
@@ -821,6 +843,22 @@ public class SDLActivity extends Activity {
         InputMethodManager imm = (InputMethodManager) SDL.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
         return imm.isAcceptingText();
 
+    }
+
+    /**
+     * This method is called by SDL using JNI.
+     */
+    public static boolean supportsRelativeMouse()
+    {
+        return SDLActivity.getMotionListener().supportsRelativeMouse();
+    }
+
+    /**
+     * This method is called by SDL using JNI.
+     */
+    public static boolean setRelativeMouseEnabled(boolean enabled)
+    {
+        return SDLActivity.getMotionListener().setRelativeMouseEnabled(enabled);
     }
 
     /**
@@ -846,6 +884,13 @@ public class SDLActivity extends Activity {
     public static boolean isAndroidTV() {
         UiModeManager uiModeManager = (UiModeManager) getContext().getSystemService(UI_MODE_SERVICE);
         return (uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION);
+    }
+
+    /**
+     * This method is called by SDL using JNI.
+     */
+    public static boolean isChromebook() {
+        return getContext().getPackageManager().hasSystemFeature("org.chromium.arc.device_management");
     }
 
     /**
@@ -880,6 +925,12 @@ public class SDLActivity extends Activity {
            Log.v("SDL", "exception " + e.toString());
         }
         return false;
+    }
+
+    // This method is called by SDLControllerManager's API 26 Generic Motion Handler.
+    public static View getContentView()
+    {
+        return mSingleton.mLayout;
     }
 
     static class ShowTextInputTask implements Runnable {
@@ -1413,7 +1464,11 @@ class SDLSurface extends SurfaceView implements SurfaceHolder.Callback,
         mSensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
 
         if (Build.VERSION.SDK_INT >= 12) {
-            setOnGenericMotionListener(new SDLGenericMotionListener_API12());
+            setOnGenericMotionListener(SDLActivity.getMotionListener());
+        }
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            setOnCapturedPointerListener(new SDLCapturedPointerListener_API26());
         }
 
         // Some arbitrary defaults to avoid a potential division by zero
@@ -1511,8 +1566,23 @@ class SDLSurface extends SurfaceView implements SurfaceHolder.Callback,
 
         mWidth = width;
         mHeight = height;
-        SDLActivity.onNativeResize(width, height, sdlFormat, mDisplay.getRefreshRate());
-        Log.v("SDL", "Window size: " + width + "x" + height);
+		int nDeviceWidth = width;
+		int nDeviceHeight = height;
+		try
+		{
+			if ( android.os.Build.VERSION.SDK_INT >= 17 )
+			{
+				android.util.DisplayMetrics realMetrics = new android.util.DisplayMetrics();
+				mDisplay.getRealMetrics( realMetrics );
+				nDeviceWidth = realMetrics.widthPixels;
+				nDeviceHeight = realMetrics.heightPixels;
+			}
+		}
+		catch ( java.lang.Throwable throwable ) {}
+
+		Log.v("SDL", "Window size: " + width + "x" + height);
+		Log.v("SDL", "Device size: " + nDeviceWidth + "x" + nDeviceHeight);
+        SDLActivity.onNativeResize(width, height, nDeviceWidth, nDeviceHeight, sdlFormat, mDisplay.getRefreshRate());
 
 
         boolean skip = false;
@@ -1715,7 +1785,14 @@ class SDLSurface extends SurfaceView implements SurfaceHolder.Callback,
                     mouseButton = 1;    // oh well.
                 }
             }
-            SDLActivity.onNativeMouse(mouseButton, action, event.getX(0), event.getY(0));
+
+            // We need to check if we're in relative mouse mode and get the axis offset rather than the x/y values
+            // if we are.  We'll leverage our existing mouse motion listener
+            SDLGenericMotionListener_API12 motionListener = SDLActivity.getMotionListener();
+            x = motionListener.getEventX(event);
+            y = motionListener.getEventY(event);
+
+            SDLActivity.onNativeMouse(mouseButton, action, x, y, motionListener.inRelativeMode());
         } else {
             switch(action) {
                 case MotionEvent.ACTION_MOVE:
@@ -1758,18 +1835,23 @@ class SDLSurface extends SurfaceView implements SurfaceHolder.Callback,
                     //White Dragon: vibrator
                     if ( SDLActivity.isVibrationEnabled() && SDLActivity.isTouchArea(action, x, y) ) {
                         int vibrationTime = 3;
-                        Vibrator vibrator = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
-                        if (vibrator.hasVibrator()) {
-                            // Start without a delay
-                            // Vibrate for 3 milliseconds
-                            // Sleep for 1000 milliseconds
-                            long[] pattern = {0, vibrationTime, 1000};
-                            // The '0' here means to repeat indefinitely
-                            // '0' is actually the index at which the pattern keeps repeating from (the start)
-                            // To repeat the pattern from any other point, you could increase the index, e.g. '1'
-                            // The '-1' here means to vibrate once, as '-1' is out of bounds in the pattern array
-                            vibrator.vibrate(pattern, -1);
-                        }
+						Vibrator vibrator = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
+						
+						if (vibrator.hasVibrator()) {
+							if (Build.VERSION.SDK_INT >= 26) {
+								vibrator.vibrate(VibrationEffect.createOneShot(vibrationTime,VibrationEffect.DEFAULT_AMPLITUDE));
+							} else {
+								// Start without a delay
+								// Vibrate for 3 milliseconds
+								// Sleep for 1000 milliseconds
+								long[] pattern = {0, vibrationTime, 1000};
+								// The '0' here means to repeat indefinitely
+								// '0' is actually the index at which the pattern keeps repeating from (the start)
+								// To repeat the pattern from any other point, you could increase the index, e.g. '1'
+								// The '-1' here means to vibrate once, as '-1' is out of bounds in the pattern array
+								vibrator.vibrate(pattern, -1);
+							}
+						}
                     }
 
                     break;
