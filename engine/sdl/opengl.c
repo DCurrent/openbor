cb5360ff9e8620c000a3b8c156244dc6cc792e97
Bryan Cain <bryancain3@gmail.com>
2019-01-16 16:52:01 -0700

    SDL: Remove dependency on SDL_gfx
    It was only being used for the framerate manager that limited the
    framerate to 200 FPS. Now that we have VSync, we don't need it.

--- engine/sdl/opengl.c
+++ engine/sdl/opengl.c
@@ -19,7 +19,6 @@
 #include "opengl.h"
 #include "video.h"
 #include "loadgl.h"
-#include "SDL2_framerate.h"
 #include <math.h>
 
 #define nextpowerof2(x) pow(2,ceil(log(x)/log(2)))
@@ -39,7 +38,6 @@ static GLfloat tcx, tcy; // maximum x and y texture coords in floating-point for
 static GLuint shaderProgram; // fragment shader program
 
 // use some variables declared in video.c that are common to both backends
-extern FPSmanager framerate_manager;
 extern int stretch;
 extern int nativeWidth, nativeHeight;
 extern SDL_Window* window;
@@ -393,11 +391,6 @@ int video_gl_copy_screen(s_videosurface* surface)
 
 	// display the rendered frame on the screen
 	SDL_GL_SwapWindow(window);
-
-#if WIN || LINUX
-	// limit framerate to 200 fps
-	SDL_framerateDelay(&framerate_manager);
-#endif
 
 	return 1;
 }
