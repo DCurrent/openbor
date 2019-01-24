4a792b67c0a5b5227de275337c5112cab838e86b
Caskey, Damon V <dc@caskeys.com>
2019-01-24 17:54:38 -0500

    Implement guard_time (guardtime) exposure.

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -386,6 +386,13 @@ HRESULT openbor_get_entity_property(ScriptVariant **varlist , ScriptVariant **pr
 
 			break;
 
+		case _ENTITY_GUARD_TIME:
+
+			ScriptVariant_ChangeType(*pretvar, VT_INTEGER);
+			(*pretvar)->lVal = (LONG)handle->guardtime;
+
+			break;
+
 		case _ENTITY_HP:
 
 			ScriptVariant_ChangeType(*pretvar, VT_INTEGER);
@@ -977,6 +984,15 @@ HRESULT openbor_set_entity_property(ScriptVariant **varlist, ScriptVariant **pre
 			if (SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
 			{
 				handle->freezetime = temp_int;
+			}
+
+			break;
+
+		case _ENTITY_GUARD_TIME:
+
+			if (SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
+			{
+				handle->guardtime = temp_int;
 			}
 
 			break;
