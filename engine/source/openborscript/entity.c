61ca06070bb76a0e581c68af771acef7fa0a9ad9
Caskey, Damon V <dc@caskeys.com>
2019-01-29 08:58:40 -0500

    Expose rise_time (staydown.rise).

--- engine/source/openborscript/entity.c
+++ engine/source/openborscript/entity.c
@@ -92,6 +92,7 @@ int mapstrings_entity_property(ScriptVariant **varlist, int paramCount)
         "projectile_prime",
 		"recursive_damage",
 		"release_time",
+		"rise_time",
 		"seal_energy",
 		"seal_time",
 		"sleep_time",
@@ -673,6 +674,13 @@ HRESULT openbor_get_entity_property(ScriptVariant **varlist , ScriptVariant **pr
 
 			ScriptVariant_ChangeType(*pretvar, VT_INTEGER);
 			(*pretvar)->lVal = (LONG)handle->releasetime;
+
+			break;
+
+		case _ENTITY_RISE_TIME:
+
+			ScriptVariant_ChangeType(*pretvar, VT_INTEGER);
+			(*pretvar)->lVal = (LONG)handle->staydown.rise;
 
 			break;
 
@@ -1411,6 +1419,15 @@ HRESULT openbor_set_entity_property(ScriptVariant **varlist, ScriptVariant **pre
 			if (SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
 			{
 				handle->releasetime = temp_int;
+			}
+
+			break;
+
+		case _ENTITY_RISE_TIME:
+
+			if (SUCCEEDED(ScriptVariant_IntegerValue(varlist[ARG_VALUE], &temp_int)))
+			{
+				handle->staydown.rise = temp_int;
 			}
 
 			break;
