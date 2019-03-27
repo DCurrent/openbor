ce0519fda42146d2e8c4d5016eee5c4513824467
Bryan Cain <bryancain3@gmail.com>
2019-03-27 11:17:41 -0600

    Remove some debug logging.

--- engine/openbor.c
+++ engine/openbor.c
@@ -2455,7 +2455,6 @@ void savesettings()
     getBasePath(path, "Saves", 0);
     getPakName(tmpname, 4);
     strcat(path, tmpname);
-    printf("save settings to %s\n", path);
     handle = fopen(path, "wb");
     if(handle == NULL)
     {
