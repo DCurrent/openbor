7d752cb14303e6fb167506ad264ed8595ef7844d
Bryan Cain <bryancain3@gmail.com>
2021-07-19 13:13:42 -0600

    Enable VSync by default and remove SDL_gfx framerate limiter.
    The SDL_gfx framerate limiter offers no advantages over VSync. Syncing
    updates to the display looks better (no risk of judder) and uses less
    power, since most displays are less than 200 Hz. (And we especially
    wouldn't want a 200 FPS limit on any displays with a higher refresh rate
    than 200 Hz.)
    
    The option to disable VSync remains intact. If you disable VSync, there
    will be no framerate cap, so you can test performance.

--- engine/sdl/video.c
+++ engine/sdl/video.c
@@ -13,7 +13,6 @@
 #else
 
 #include "sdlport.h"
-#include "SDL2_framerate.h" //Kratus (29-04-21) Reversed the FPS limit to reduce CPU usage
 #include <math.h>
 #include "types.h"
 #include "video.h"
@@ -30,7 +29,6 @@
 SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
-FPSmanager framerate_manager; //Kratus (29-04-21) Reversed the FPS limit to reduce CPU usage
 s_videomodes stored_videomodes;
 yuv_video_mode stored_yuv_mode;
 int yuv_mode = 0;
@@ -68,8 +66,6 @@ void initSDL()
 	nativeWidth = video_info.w;
 	nativeHeight = video_info.h;
 	printf("debug:nativeWidth, nativeHeight, bpp, Hz  %d, %d, %d, %d\n", nativeWidth, nativeHeight, SDL_BITSPERPIXEL(video_info.format), video_info.refresh_rate);
-	SDL_initFramerate(&framerate_manager);
-	SDL_setFramerate(&framerate_manager, 200); //Kratus (29-04-21) Reversed the FPS limit to reduce CPU usage
 }
 
 void video_set_window_title(const char* title)
@@ -227,11 +223,6 @@ int video_copy_screen(s_screen* src)
 
 	SDL_UpdateTexture(texture, NULL, surface->data, surface->pitch);
 	blit();
-
-	//Kratus (29-04-21) Reversed the FPS limit to reduce CPU usage
-	#if WIN || LINUX
-	SDL_framerateDelay(&framerate_manager);
-	#endif
 
 	return 1;
 }
