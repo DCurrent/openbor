ef757d3c0ce6012d310bae95736df4e19df454bc
Caskey, Damon V <dc@caskeys.com>
2021-07-20 01:05:31 -0400

    Restore e_key_id enum accidently while importing master branch to dc updates.

--- engine/openbor.h
+++ engine/openbor.h
@@ -447,6 +447,31 @@ typedef struct
 // Key definitions.
 typedef enum
 {
+    FLAG_ESC = (1 << 0),
+    FLAG_START = (1 << 1),
+    FLAG_MOVELEFT = (1 << 2),
+    FLAG_MOVERIGHT = (1 << 3),
+    FLAG_MOVEUP = (1 << 4),
+    FLAG_MOVEDOWN = (1 << 5),
+    FLAG_ATTACK = (1 << 6),
+    FLAG_JUMP = (1 << 7),
+    FLAG_SPECIAL = (1 << 8),
+    FLAG_SCREENSHOT = (1 << 9),
+    FLAG_ATTACK2 = (1 << 10),
+    FLAG_ATTACK3 = (1 << 11),
+    FLAG_ATTACK4 = (1 << 12),
+    FLAG_ANYBUTTON = (FLAG_START | FLAG_SPECIAL | FLAG_ATTACK | FLAG_ATTACK2 | FLAG_ATTACK3 | FLAG_ATTACK4 | FLAG_JUMP),
+    FLAG_CONTROLKEYS = (FLAG_SPECIAL | FLAG_ATTACK | FLAG_ATTACK2 | FLAG_ATTACK3 | FLAG_ATTACK4 | FLAG_JUMP | FLAG_MOVEUP | FLAG_MOVEDOWN | FLAG_MOVELEFT | FLAG_MOVERIGHT),
+    FLAG_FORWARD = (1 << 13),
+    FLAG_BACKWARD = (1 << 14)
+} e_key_def;
+
+// Caskey, Damon V
+// 2013-12-27
+//
+// Key definitions.
+typedef enum
+{
     SDID_MOVEUP,
     SDID_MOVEDOWN,
     SDID_MOVELEFT,
