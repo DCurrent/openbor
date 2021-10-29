3030432cf38731c4abee5c886a1be0e4d2ae3eae
fgames9000 <fgames9000@gmail.com>
2021-10-29 05:48:30 -0300

    October-2021 Updates
    - Added the new "jumpspecial" property that allows the modder to enable or disable the default animation movement.

--- engine/source/openborscript/commands.c
+++ engine/source/openborscript/commands.c
@@ -196,6 +196,7 @@ List *createModelCommandList(void)
         LIST_ADD(CMD_MODEL_COLLISION_ETC, buf);
     }
 
+    // Kratus (10-2021) Added new jumpspecial property
     LIST_ADD(CMD_MODEL_COLLISIONONE, "attackone");
     LIST_ADD(CMD_MODEL_ATTACKTHROTTLE, "attackthrottle");
     LIST_ADD(CMD_MODEL_COLLISIONZ, "attackz");
@@ -336,6 +337,7 @@ List *createModelCommandList(void)
     LIST_ADD(CMD_MODEL_JUMPFRAME, "jumpframe");
     LIST_ADD(CMD_MODEL_JUMPHEIGHT, "jumpheight");
     LIST_ADD(CMD_MODEL_JUMPMOVE, "jumpmove");
+    LIST_ADD(CMD_MODEL_JUMPSPECIAL, "jumpspecial");
     LIST_ADD(CMD_MODEL_JUMPSPEED, "jumpspeed");
     LIST_ADD(CMD_MODEL_JUMPSPEEDF, "jumpspeedf");
     LIST_ADD(CMD_MODEL_KEYSCRIPT, "keyscript");
