22f2f868ee4da62aeae733a4ce034964caf86485
DCurrent <dc@caskeys.com>
2024-02-06 09:34:49 -0500

    Revert "glibc missing fix"
    This reverts commit e5b4253256959c02282adeac475d8780045d6f5f.

--- engine/source/utils.c
+++ engine/source/utils.c
@@ -308,11 +308,9 @@ void *checkAlloc(void *ptr, size_t size, const char *func, const char *file, int
         writeToLogFile("Out of memory!\n");
         writeToLogFile("Allocation of size %i failed in function '%s' at %s:%i.\n", size, func, file, line);
 #ifndef WIN
-    #if __GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 33)
-    writeToLogFile("Memory usage at exit: %u\n", mallinfo2().arena);
-    #else
-    writeToLogFile("Memory usage at exit: %u\n", mallinfo().arena);
-    #endif
+        writeToLogFile("Memory usage at exit: %u\n", mallinfo().arena);
+#elif LINUX
+        writeToLogFile("Memory usage at exit: %u\n", mallinfo2().arena);
 #endif
         borExit(2);
     }
