2aba6805ed8e1fdf0e5ac279a122a7e6684d3f6e
pcabral <paulocabral@me.com>
2024-04-07 18:54:57 -0400

    Added support for x86,AMD64,MMX detection. Linux Port should work for all cpu targets.

--- cmake/linux.cmake
+++ cmake/linux.cmake
@@ -1,10 +1,20 @@
 set(COMMON_COMPILER_FLAGS "${COMMON_COMPILER_FLAGS} -Wno-deprecated-declarations -Wno-void-pointer-to-enum-cast")
 
 set(BUILD_SDL     ON)
+set(BUILD_SDL_IO  ON)
+set(BUILD_OPENGL  ON)
+set(BUILD_LOADGL  ON)
 set(BUILD_GFX     ON)
 set(BUILD_VORBIS  ON)
 set(BUILD_WEBM    ON)
 set(BUILD_PTHREAD ON)
+
+if (CMAKE_SYSTEM_PROCESSOR MATCHES "(amd64)|(AMD64)")
+  add_definitions(-DELF -DAMD64)
+elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)")
+  add_definitions(-DELF)
+  set(BUILD_MMX ON)
+endif()
 
 add_definitions(-DLINUX)
 
