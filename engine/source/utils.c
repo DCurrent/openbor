1de99c0debe5d2e3a28a9afa9bede34df8f5c7b6
pcabral <paulocabral@me.com>
2024-04-09 16:18:53 -0400

    Restored memory usage for Darwin

--- engine/source/utils.c
+++ engine/source/utils.c
@@ -309,12 +309,10 @@ void *checkAlloc(void *ptr, size_t size, const char *func, const char *file, int
                        "\n*            Shutting Down            *\n\n");
         writeToLogFile("Out of memory!\n");
         writeToLogFile("Allocation of size %i failed in function '%s' at %s:%i.\n", size, func, file, line);
-#ifndef DARWIN
-#ifndef WIN
-        writeToLogFile("Memory usage at exit: %u\n", mallinfo().arena);
-#elif LINUX
+#if LINUX && !DARWIN
         writeToLogFile("Memory usage at exit: %u\n", mallinfo2().arena);
-#endif
+#else
+        writeToLogFile("Memory usage at exit: %u\n", getUsedRam(BYTES));
 #endif
         borExit(2);
     }
