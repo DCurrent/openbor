2a62b1bc343ffdc3b3f9f2c85730e6b9498ea6b5
Damon V. Caskey <dc@caskeys.com>
2026-08-11 00:07:18 -0400

    Remove obsolete i386 Vorbis FPU path

--- engine/source/webmlib/vorbisfpu.h
+++ engine/source/webmlib/vorbisfpu.h
@@ -36,44 +36,6 @@
 #include <ogg/os_types.h>
 #include <math.h>
 
-/* Special i386 GCC implementation */
-#if defined(__i386__) && defined(__GNUC__) && !defined(__BEOS__)
-#  define VORBIS_FPU_CONTROL
-/* both GCC and MSVC are kinda stupid about rounding/casting to int.
-   Because of encapsulation constraints (GCC can't see inside the asm
-   block and so we end up doing stupid things like a store/load that
-   is collectively a noop), we do it this way */
-
-/* we must set up the fpu before this works!! */
-
-typedef ogg_int16_t vorbis_fpu_control;
-
-static inline void vorbis_fpu_setround(vorbis_fpu_control *fpu){
-  ogg_int16_t ret;
-  ogg_int16_t temp;
-  __asm__ __volatile__("fnstcw %0\n\t"
-          "movw %0,%%dx\n\t"
-          "andw $62463,%%dx\n\t"
-          "movw %%dx,%1\n\t"
-          "fldcw %1\n\t":"=m"(ret):"m"(temp): "dx");
-  *fpu=ret;
-}
-
-static inline void vorbis_fpu_restore(vorbis_fpu_control fpu){
-  __asm__ __volatile__("fldcw %0":: "m"(fpu));
-}
-
-/* assumes the FPU is in round mode! */
-static inline int vorbis_ftoi(double f){  /* yes, double!  Otherwise,
-                                             we get extra fst/fld to
-                                             truncate precision */
-  int i;
-  __asm__("fistl %0": "=m"(i) : "t"(f));
-  return(i);
-}
-#endif /* Special i386 GCC implementation */
-
-
 /* Optimized code path for x86_64 builds. Uses SSE2 intrinsics. This can be
    done safely because all x86_64 CPUs supports SSE2. */
 #if (defined(_MSC_VER) && defined(_WIN64)) || (defined(__GNUC__) && defined (__x86_64__))
