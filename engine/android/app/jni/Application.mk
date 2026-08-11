c4c83bfba85627801394223cd670ecd7a34f8049
Damon V. Caskey <dc@caskeys.com>
2026-08-10 23:47:19 -0400

    Restrict Android NDK build to ARM64

--- engine/android/app/jni/Application.mk
+++ engine/android/app/jni/Application.mk
@@ -3,8 +3,7 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 # APP_STL := stlport_static 
 
-#arm64-v8a - unsupported for now.
-APP_ABI := armeabi-v7a arm64-v8a
+APP_ABI := arm64-v8a
 
 # Min SDK level
 APP_PLATFORM=android-16
