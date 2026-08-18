bccd98a51980fec3a45530ecc565bf17d4d8abf6
Damon V. Caskey <dc@caskeys.com>
2026-08-18 14:52:06 -0400

    Use portable Linux linker defaults

--- cmake/linux.cmake
+++ cmake/linux.cmake
@@ -1,22 +1,8 @@
 set(COMMON_COMPILER_FLAGS "${COMMON_COMPILER_FLAGS} -Wno-stringop-truncation -Wno-maybe-uninitialized -Wno-unused-result -Wno-void-pointer-to-enum-cast -Wno-deprecated-declarations")
 set(ARCH_SUFFIX "")
 
-set(USE_SDL     ON)
-set(USE_OPENGL  ON)
-set(USE_LOADGL  ON)
-set(USE_GFX     ON)
-set(USE_VORBIS  ON)
-set(USE_WEBM    ON)
-set(USE_PTHREAD ON)
-
-set(ENABLE_STATIC ON)
-
 if(NOT CMAKE_PREFIX_PATH)
     set(CMAKE_PREFIX_PATH "/usr")
-endif()
-
-if(NOT CMAKE_LIBRARY_PATH)
-    set(CMAKE_LIBRARY_PATH "${CMAKE_PREFIX_PATH}/lib")
 endif()
 
 if(TARGET_ARCH STREQUAL "arm64")
@@ -28,9 +14,6 @@ if(NOT TARGET_ARCH MATCHES "${CMAKE_SYSTEM_PROCESSOR}")
     if(NOT CMAKE_C_COMPILER)
       set(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc-12")
     endif()
-    if(NOT CMAKE_LIBRARY_PATH)
-      set(CMAKE_LIBRARY_PATH "${CMAKE_PREFIX_PATH}/usr/lib/aarch64-linux-gnu")
-    endif()
   endif()
 endif()
 
@@ -41,12 +24,11 @@ target_include_directories(${PROJECT_NAME} PRIVATE
   ${CMAKE_PREFIX_PATH}/include/SDL2
 )
 
-string(REPLACE " " ";" library_paths ${CMAKE_LIBRARY_PATH})
-foreach(lib_path ${library_paths})
-  target_link_libraries(${PROJECT_NAME} PRIVATE
-    -L${lib_path}
+if(CMAKE_LIBRARY_PATH)
+  target_link_directories(${PROJECT_NAME} PRIVATE
+    ${CMAKE_LIBRARY_PATH}
   )
-endforeach()
+endif()
 
 # Distribution Preperation
 add_custom_command(TARGET ${PROJECT_NAME}
